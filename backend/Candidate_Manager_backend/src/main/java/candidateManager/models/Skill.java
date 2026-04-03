d95918cbc9cf83661813d9438626d196bb5f4fa8
Lazar <lazarcima@gmail.com>
2026-04-03 18:21:43 +0200

    modify skill entity

--- backend/Candidate_Manager_backend/src/main/java/candidateManager/models/Skill.java
+++ backend/Candidate_Manager_backend/src/main/java/candidateManager/models/Skill.java
@@ -28,9 +28,14 @@ public class Skill implements Serializable {
     }
 
     public int getId() { return id; }
+    
     public void setId(int id) { this.id = id; }
+    
     public String getName() { return name; }
+    
     public void setName(String name) { this.name = name; }
+    
     public List<Candidate> getCandidates() { return candidates; }
+    
     public void setCandidates(List<Candidate> candidates) { this.candidates = candidates; }
 }
