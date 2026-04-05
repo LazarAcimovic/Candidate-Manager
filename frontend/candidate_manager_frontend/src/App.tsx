31ca5e4ce456ba3d0994439c1abad7d1686c4411
Lazar <lazarcima@gmail.com>
2026-04-05 05:51:58 +0200

    modify app.tsx

--- frontend/candidate_manager_frontend/src/App.tsx
+++ frontend/candidate_manager_frontend/src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app-container">
+      <main className="content-area">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default App;
