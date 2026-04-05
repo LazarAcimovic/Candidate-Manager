ccf4f11c08e52dad195a107208e140bcdf9fe322
Lazar <lazarcima@gmail.com>
2026-04-05 06:14:40 +0200

    add navbar to app.tsx

--- frontend/candidate_manager_frontend/src/App.tsx
+++ frontend/candidate_manager_frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app-container">
+    <Navbar/>
       <main className="content-area">
         <Outlet />
       </main>
