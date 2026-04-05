e09062c0869d095b6d1fb3686a879c588c71c982
Lazar <lazarcima@gmail.com>
2026-04-05 08:27:42 +0200

    change url to apiClient.ts

--- frontend/candidate_manager_frontend/src/services/apiClient.ts
+++ frontend/candidate_manager_frontend/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8793/api",
+  baseURL: "http://localhost:8793",
   headers: {
     "Content-Type": "application/json",
   },
